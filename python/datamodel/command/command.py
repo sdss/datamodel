7bbbb7b4dd9d12bd2753e53f2bd24ec81351f967
Joel Brownstein <joelbrownstein@sdss.org>
2020-11-09 19:35:30 -0700

    generate

--- python/datamodel/command/command.py
+++ python/datamodel/command/command.py
@@ -13,7 +13,7 @@ from os import getenv
 class Command:
 
     def __init__(self, name=None):
-        name = sysv[0] if name is None else name
+        name = argv[0] if name is None else name
         self.get_options = globals()[name] if name in globals().keys() else None
         self.options = self.get_options() if self.get_options else None
         self.options._name = name if self.options else None
