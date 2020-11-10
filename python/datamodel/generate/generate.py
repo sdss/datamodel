3aaa11d241a3abec5796c6e88ba3dc340ef02cd6
Joel Brownstein <joelbrownstein@sdss.org>
2020-11-09 19:43:26 -0700

    generate

--- python/datamodel/generate/generate.py
+++ python/datamodel/generate/generate.py
@@ -8,5 +8,14 @@
 
 class Generate:
 
-    def __init__(self, options = None, verbose = None):
+    def __init__(self, options = None, tree_ver = None, location = None, env_label = None, force = None, verbose = None, debug = None, nolog = None):
+        self.tree_ver = options.tree_ver if options else tree_ver
+        self.location = options.location if options else location
+        self.env_label = options.env_label if options else env_label
+        self.force = options.verbose if options else force
         self.verbose = options.verbose if options else verbose
+        self.debug = options.debug if options else debug
+        self.nolog = options.nolog if options else nolog
+
+    def set_file(self):
+        pass
