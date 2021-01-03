a0bf0f82927265ede8f1036df7b73fd701fdaa36
Joel Brownstein <joelbrownstein@astro.utah.edu>
2021-01-03 00:08:30 -0700

    wiki

--- python/datamodel/wiki/page.py
+++ python/datamodel/wiki/page.py
@@ -35,13 +35,12 @@ class Page(object):
     location: str, optional (required w/out path)
         the partial symbolic path (can be parsed from the
         symbolic path if omitted)
-    format : str, either md (default) or html
     force : bool (default false)
         set True to over-write the human edited yaml and
         restore it to the original template / fits file.
     """
     
-    def __init__(self, options = None, tree_ver = None, space_ver = None, file_spec = None, path = None, env_label = None, location = None, format = None, force = None, verbose = None, debug = None):
+    def __init__(self, options = None, tree_ver = None, space_ver = None, file_spec = None, path = None, env_label = None, location = None, force = None, verbose = None, debug = None):
         self.tree_ver = options.tree_ver if options else tree_ver
         self.space_ver = options.space_ver if options else space_ver
         if self.space_ver is None: self.space_ver = "sdsswork"
@@ -56,7 +55,6 @@ class Page(object):
             except:
                 self.env_label = options.env_label if options else env_label
                 self.location = options.location if options else location
-        self.format = options.format if options else format
         self.force = options.force if options else force
         self.verbose = options.verbose if options else verbose
         self.debug = options.debug if options else debug
