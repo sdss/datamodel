af6880d48c8c49d113a70c4a17b2ca1eea4c920b
Brian Cherinka <havok2063@hotmail.com>
2021-06-25 16:12:45 -0400

    adding lazy loading to data products list

--- python/datamodel/products/product.py
+++ python/datamodel/products/product.py
@@ -23,20 +23,31 @@ products_path = pathlib.Path(os.getenv("DATAMODEL_DIR")) / 'datamodel' / 'produc
 class Product(object):
     _extract = ['short', 'description']
     
-    def __init__(self, name: str):
+    def __init__(self, name: str, load: bool = False):
 
         self.name = name
         self.path = products_path / f"{name}.json"
-        self._model = ProductModel.parse_file(self.path)
-            
-        self.releases = self._model.general.releases
+        self.loaded = False
         
-        for field in self._extract:
-            setattr(self, field, getattr(self._model.general, field))
+        if load:
+            self._load_product_from_model()
 
     def __repr__(self):
-        return f'<Product ("{self.name}", summary="{self.short}")>'
+        short = hasattr(self, 'short')
+        return f'<Product ("{self.name}", summary="{self.short if short else ""}")>'
     
+    def load(self):
+        if self.loaded:
+            return 
+        self._load_product_from_model()
+
+    def _load_product_from_model(self):
+        self._model = ProductModel.parse_file(self.path)
+        self.releases = self._model.general.releases        
+        for field in self._extract:
+            setattr(self, field, getattr(self._model.general, field))
+        self.loaded = True
+
     def get_release(self, value: str):
 
         if value not in self.releases:
@@ -48,20 +59,34 @@ class Product(object):
         return self._model.dict()
     
     @classmethod
-    def from_file(cls, value: Union[str, pathlib.Path]):
+    def from_file(cls, value: Union[str, pathlib.Path], load: bool = None):
         path = pathlib.Path(value)
-        return cls(name=path.stem)
+        return cls(name=path.stem, load=load)
     
     
 class DataProducts(list):
     
     def __init__(self):
-        list.__init__(self, [Product.from_file(i) for i in products_path.rglob('*.json')])
+        list.__init__(self, [Product.from_file(i, load=False) for i in products_path.rglob('*.json')])
         
     def __repr__(self):
         return f'<DataProducts (n_products={len(self)})>'
-    
-    
+
+    def __getitem__(self, item):
+        if isinstance(item, str) and item in self:
+            vals = [i.name for i in self]
+            obj =  list.__getitem__(self, vals.index(item))
+            obj.load()
+            return obj
+        obj = list.__getitem__(self, item)
+        obj.load()
+        return obj
+
+    def __contains__(self, value):
+        if isinstance(value, str):
+            return value in [i.name for i in self]
+        return value in self
+
 class DataModel(object):
     def __init__(self):
         self.releases = releases
