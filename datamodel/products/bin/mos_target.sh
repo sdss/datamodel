bf9db25d1e8129de33edf15787cd838689ca4ed2
Joel Brownstein <joelbrownstein@astro.utah.edu>
2022-09-26 17:30:07 -0600

    mos target with summary txt

--- datamodel/products/bin/mos_target.sh
+++ datamodel/products/bin/mos_target.sh
@@ -9,7 +9,6 @@ datamodel_generate -f mos_target_allwise -p MOS_TARGET/{v_targ}/mos_target_allwi
 datamodel_generate -f mos_target_carton_to_target -p MOS_TARGET/{v_targ}/mos_target_carton_to_target-{num}.fits -k v_targ=1.0.1 -k num=01 -t sdss5 --skip-git
 datamodel_generate -f mos_target_catwise2020 -p MOS_TARGET/{v_targ}/mos_target_catwise2020-{num}.fits -k v_targ=1.0.1 -k num=01 -t sdss5 --skip-git
 datamodel_generate -f mos_target_magnitude -p MOS_TARGET/{v_targ}/mos_target_magnitude-{num}.fits -k v_targ=1.0.1 -k num=01 -t sdss5 --skip-git
-datamodel_generate -f mos_target_bhm_efeds_veto -p MOS_TARGET/{v_targ}/mos_target_bhm_efeds_veto.fits -k v_targ=1.0.1 -t sdss5 --skip-git
 datamodel_generate -f mos_target_bhm_rm_v0_2_1 -p MOS_TARGET/{v_targ}/mos_target_bhm_rm_v0_2_1.fits -k v_targ=1.0.1 -t sdss5 --skip-git
 datamodel_generate -f mos_target_cadence_epoch -p MOS_TARGET/{v_targ}/mos_target_cadence_epoch.fits -k v_targ=1.0.1 -t sdss5 --skip-git
 datamodel_generate -f mos_target_cadence -p MOS_TARGET/{v_targ}/mos_target_cadence.fits -k v_targ=1.0.1 -t sdss5 --skip-git
@@ -19,3 +18,14 @@ datamodel_generate -f mos_target_category -p MOS_TARGET/{v_targ}/mos_target_cate
 datamodel_generate -f mos_target_instrument -p MOS_TARGET/{v_targ}/mos_target_instrument.fits -k v_targ=1.0.1 -t sdss5 --skip-git
 datamodel_generate -f mos_target_mapper -p MOS_TARGET/{v_targ}/mos_target_mapper.fits -k v_targ=1.0.1 -t sdss5 --skip-git
 datamodel_generate -f mos_target_targetdb_version -p MOS_TARGET/{v_targ}/mos_target_targetdb_version.fits -k v_targ=1.0.1 -t sdss5 --skip-git
+
+	modified:   mos_target_bhm_efeds_veto.yaml
+	modified:   mos_target_cadence.yaml
+	modified:   mos_target_cadence_epoch.yaml
+	modified:   mos_target_carton.yaml
+	modified:   mos_target_carton_to_target.yaml
+	modified:   mos_target_category.yaml
+	modified:   mos_target_catwise2020.yaml
+	modified:   mos_target_instrument.yaml
+	modified:   mos_target_magnitude.yaml
+	modified:   mos_target_mapper.yaml
