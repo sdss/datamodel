0e4db97f7c6449e9dd84c43324406a349737539e
Joel Brownstein <joelbrownstein@astro.utah.edu>
2025-03-19 17:13:51 -0600

    mos target dr19

--- bin/examples/mos-target/dr19/mos_target.sh
+++ bin/examples/mos-target/dr19/mos_target.sh
@@ -8,7 +8,7 @@ datamodel_generate -f mos_target_bhm_rm_v0_2 -p MOS_TARGET/{v_targ}/mos_bhm_rm_v
 datamodel_generate -f mos_target_cadence -p MOS_TARGET/{v_targ}/mos_cadence.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_cadence_epoch -p MOS_TARGET/{v_targ}/mos_cadence_epoch.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 ?? datamodel_generate -f mos_target_carton -p MOS_TARGET/{v_targ}/mos_carton.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_carton_to_target -p MOS_TARGET/{v_targ}/mos_carton_to_target-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
+datamodel_generate -f mos_target_carton_to_target -p MOS_TARGET/{v_targ}/mos_carton_to_target-{num}.fits -k v_targ=1.0.2 -k num=001 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_cataclysmic_variables -p MOS_TARGET/{v_targ}/mos_cataclysmic_variables.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog -p MOS_TARGET/{v_targ}/mos_catalog-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_allwise -p MOS_TARGET/{v_targ}/mos_catalog_to_allwise-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
@@ -16,16 +16,16 @@ datamodel_generate -f mos_target_catalog_to_bhm_efeds_veto -p MOS_TARGET/{v_targ
 datamodel_generate -f mos_target_catalog_to_bhm_rm_v0_2 -p MOS_TARGET/{v_targ}/mos_catalog_to_bhm_rm_v0_2.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_catwise2020 -p MOS_TARGET/{v_targ}/mos_catalog_to_catwise2020-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_glimpse -p MOS_TARGET/{v_targ}/mos_catalog_to_glimpse-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_catalog_to_guvcat -p MOS_TARGET/{v_targ}/mos_catalog_to_guvcat-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_catalog_to_legacy_survey_dr8 -p MOS_TARGET/{v_targ}/mos_catalog_to_legacy_survey_dr8-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
+datamodel_generate -f mos_target_catalog_to_guvcat -p MOS_TARGET/{v_targ}/mos_catalog_to_guvcat-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
+datamodel_generate -f mos_target_catalog_to_legacy_survey_dr8 -p MOS_TARGET/{v_targ}/mos_catalog_to_legacy_survey_dr8-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_panstarrs1 -p MOS_TARGET/{v_targ}/mos_catalog_to_panstarrs1-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_catalog_to_sdss_dr13_photoobj_primary -p MOS_TARGET/{v_targ}/mos_catalog_to_sdss_dr13_photoobj_primary-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_catalog_to_sdss_dr16_specobj -p MOS_TARGET/{v_targ}/mos_catalog_to_sdss_dr16_specobj.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
+datamodel_generate -f mos_target_catalog_to_sdss_dr13_photoobj_primary -p MOS_TARGET/{v_targ}/mos_catalog_to_sdss_dr13_photoobj_primary-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
+datamodel_generate -f mos_target_catalog_to_sdss_dr16_specobj -p MOS_TARGET/{v_targ}/mos_catalog_to_sdss_dr16_specobj-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_skies_v2 -p MOS_TARGET/{v_targ}/mos_catalog_to_skies_v2-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_skymapper_dr2 -p MOS_TARGET/{v_targ}/mos_catalog_to_skymapper_dr2-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_supercosmos -p MOS_TARGET/{v_targ}/mos_catalog_to_supercosmos-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_tic_v8 -p MOS_TARGET/{v_targ}/mos_catalog_to_tic_v8-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_catalog_to_tycho2 -p MOS_TARGET/{v_targ}/mos_catalog_to_tycho2.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
+datamodel_generate -f mos_target_catalog_to_tycho2 -p MOS_TARGET/{v_targ}/mos_catalog_to_tycho2-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_uvotssc1 -p MOS_TARGET/{v_targ}/mos_catalog_to_uvotssc1.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_catalog_to_xmm_om_suss_4_1 -p MOS_TARGET/{v_targ}/mos_catalog_to_xmm_om_suss_4_1.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 ?? datamodel_generate -f mos_target_catalogdb_version -p MOS_TARGET/{v_targ}/mos_catalogdb_version.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
@@ -49,7 +49,7 @@ datamodel_generate -f mos_target_sagitta -p MOS_TARGET/{v_targ}/mos_sagitta.fits
 datamodel_generate -f mos_target_sdss_apogeeallstarmerge_r13 -p MOS_TARGET/{v_targ}/mos_sdss_apogeeallstarmerge_r13.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_sdss_dr13_photoobj_primary -p MOS_TARGET/{v_targ}/mos_sdss_dr13_photoobj_primary-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
 ? datamodel_generate -f mos_target_sdss_dr16_qso -p MOS_TARGET/{v_targ}/mos_sdss_dr16_qso.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_sdss_dr16_specobj -p MOS_TARGET/{v_targ}/mos_sdss_dr16_specobj.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
+datamodel_generate -f mos_target_sdss_dr16_specobj -p MOS_TARGET/{v_targ}/mos_sdss_dr16_specobj-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_skies_v2 -p MOS_TARGET/{v_targ}/mos_skies_v2-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_skymapper_dr2 -p MOS_TARGET/{v_targ}/mos_skymapper_dr2-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_supercosmos -p MOS_TARGET/{v_targ}/mos_supercosmos-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
@@ -58,7 +58,7 @@ datamodel_generate -f mos_target_target -p MOS_TARGET/{v_targ}/mos_target-{num}.
 ?? datamodel_generate -f mos_target_targeting_generation -p MOS_TARGET/{v_targ}/mos_targeting_generation.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_tic_v8 -p MOS_TARGET/{v_targ}/mos_tic_v8-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_twomass_psc -p MOS_TARGET/{v_targ}/mos_twomass_psc-{num}.fits -k v_targ=1.0.2 -k num=01 -r DR19 -c DR18 -h
-?? datamodel_generate -f mos_target_tycho2 -p MOS_TARGET/{v_targ}/mos_tycho2.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
+? datamodel_generate -f mos_target_tycho2 -p MOS_TARGET/{v_targ}/mos_tycho2-{num}.fits -k v_targ=1.0.2 -k num=1 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_uvotssc1 -p MOS_TARGET/{v_targ}/mos_uvotssc1.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_xmm_om_suss_4_1 -p MOS_TARGET/{v_targ}/mos_xmm_om_suss_4_1.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 datamodel_generate -f mos_target_yso_clustering -p MOS_TARGET/{v_targ}/mos_yso_clustering.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
