099225b6c8a539b846a68e9004aed2dfcb4bcb7d
Joel Brownstein <joelbrownstein@astro.utah.edu>
2025-03-20 10:18:11 -0600

    mos target dr19

--- bin/examples/mos-target/dr19/mos_target_exceptions.sh
+++ bin/examples/mos-target/dr19/mos_target_exceptions.sh
@@ -1,8 +1,4 @@
-?? datamodel_generate -f mos_target_carton -p MOS_TARGET/{v_targ}/mos_carton.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
-needs a dr18 datamodel?
 
 ?? datamodel_generate -f mos_target_targetdb_version -p MOS_TARGET/{v_targ}/mos_targetdb_version.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
 needs a DR18 datamodel
 ?? datamodel_generate -f mos_target_targeting_generation -p MOS_TARGET/{v_targ}/mos_targeting_generation.fits -k v_targ=1.0.2 -r DR19 -c DR18 -h
-
-
