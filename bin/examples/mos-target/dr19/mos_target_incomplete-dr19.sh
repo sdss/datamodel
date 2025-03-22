534e342813b24aa3ad45dd1ebcf017026c82ac89
Joel Brownstein <joelbrownstein@astro.utah.edu>
2025-03-22 11:48:27 -0600

    mos target

---
+++ bin/examples/mos-target/dr19/mos_target_incomplete-dr19.sh
@@ -0,0 +1,1 @@
+datamodel_generate -f mos_target_allstar_dr17_synspec_rev1_1 -p MOS_TARGET/{v_targ}/mos_allstar_dr17_synspec_rev1.fits -k v_targ=1.0.2 -r DR19
--- bin/examples/mos-target/dr19/mos_target_new-dr19.sh
+++ bin/examples/mos-target/dr19/mos_target_new-dr19.sh
@@ -1,5 +1,6 @@
-datamodel_generate -f mos_target_allstar_dr17_synspec_rev1_1 -p MOS_TARGET/{v_targ}/mos_allstar_dr17_synspec_rev1.fits -k v_targ=1.0.2 -r DR19
 datamodel_generate -f mos_target_bhm_csc -p MOS_TARGET/{v_targ}/mos_bhm_csc.fits -k v_targ=1.0.2 -r DR19
+datamodel_update -f mos_target_bhm_csc -r DR19
+
 datamodel_generate -f mos_target_bhm_rm_tweaks -p MOS_TARGET/{v_targ}/mos_bhm_rm_tweaks.fits -k v_targ=1.0.2 -r DR19
 datamodel_generate -f mos_target_bhm_rm_v0_1 -p MOS_TARGET/{v_targ}/mos_bhm_rm_v0_1.fits -k v_targ=1.0.2 -r DR19
 datamodel_generate -f mos_target_bhm_spiders_agn_superset -p MOS_TARGET/{v_targ}/mos_bhm_spiders_agn_superset.fits -k v_targ=1.0.2 -r DR19
